2fa81b1706d98801fb75efa5a5b996b1a44c658e
huzeyfeyer <huzeyfeyer1@gmail.com>
2025-05-24 23:02:30 +0200

    Middleware 'isAdmin' toegevoegd en admin toegang beveiligd

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,10 +12,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/admin', function () {
+    return view('admin.dashboard'); // Admin dashboard view
+})->middleware(['auth', 'isAdmin']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'isAdmin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index']);
 });
 
 require __DIR__.'/auth.php';
